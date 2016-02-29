217ffaa11a4ca71b0e73e414cc08e63c42e2347d
jrfcabral <joaofigueiredocabral@gmail.com>
2016-02-29 12:12:02 +0000

    Implemented server mc functionality.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,20 @@
 package server;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Server {
 	
 
 	public static void main(String[] args) {
-		ServerThread t = new ServerThread(Integer.parseInt(args[0]));
+		InetAddress mcgroup;
+		try {
+			 mcgroup = InetAddress.getByName(args[1]);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			return;
+		}
+		ServerThread t = new ServerThread(Integer.parseInt(args[0]), mcgroup, Integer.parseInt(args[2]));
 		t.run();		
 	}
 
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -3,23 +3,41 @@ package server;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Optional;
 
 public class ServerThread extends Thread {
 	private static final String NOT_FOUND = "NOT_FOUND";
 	private DatagramSocket socket;
+	private MulticastSocket mcSocket;
+	private InetAddress mcgroup;
 	private HashMap<String, String> register;
-	
-	public ServerThread(int port){
+	private int mcport;
+	private int port;
+		
+	public ServerThread(int servicePort, InetAddress multicastAddress, int multicastPort){
 		try {
-			this.socket = new DatagramSocket(port);
+			this.port = servicePort;
+			this.socket = new DatagramSocket(servicePort);
+			this.mcSocket = new MulticastSocket(multicastPort);
 			this.register = new HashMap<String, String>();
-		} catch (SocketException e) {		
+			this.mcgroup = multicastAddress;
+			this.mcport = multicastPort;
+		} catch (Exception e) {		
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			mcSocket.joinGroup(this.mcgroup);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
 	
 	private boolean validatePlate(String plate){
 		return plate.matches("[a-zA-Z0-9]{2}-[a-zA-Z0-9]{2}-[a-zA-Z0-9]{2}") && this.register.get(plate) == null;		
@@ -35,7 +53,7 @@ public class ServerThread extends Thread {
 			if (validatePlate(args[1])){
 				this.register.put(args[1], args[2]);
 				System.out.println(this.register.get(args[1]));
-				boolean value = args[1].equals("25-22-26");
+				args[1].equals("25-22-26");
 				return Integer.toString(register.size());
 			}
 			else return Integer.toString(-1);
@@ -51,12 +69,27 @@ public class ServerThread extends Thread {
 			return null;
 	}
 	
+	private void announce(){
+		try {
+			String ipaddress = InetAddress.getLocalHost().getHostAddress() + " " +Integer.toString(this.port);
+			byte[] buf = ipaddress.getBytes();			
+			DatagramPacket announcePacket = new DatagramPacket(buf, buf.length, this.mcgroup, this.mcport);
+			this.mcSocket.send(announcePacket);
+			System.out.println("multicast: "+ this.mcgroup.getHostAddress() + " " + Integer.toString(this.mcport) +": " + InetAddress.getLocalHost().getHostAddress() + " " + Integer.toString(this.port));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void run(){
 		
 		while(true){
 			try{
+				
 				byte[] buf = new byte[512];
 				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				this.socket.setSoTimeout(1000);
 				this.socket.receive(packet);
 				int length = packet.getLength();
 				
@@ -67,6 +100,10 @@ public class ServerThread extends Thread {
 				byte[] bufOut = reply.getBytes();
 				DatagramPacket replyPacket = new DatagramPacket(bufOut, bufOut.length, packet.getAddress(), packet.getPort());
 				this.socket.send(replyPacket);
+			}
+			catch(SocketTimeoutException t){
+				this.announce();
+				continue;
 			}
 			catch(IOException e){
 				break;
